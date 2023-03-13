e1032edbdc48cc3be3454e2ae9af17bacd403dee
MikitaYurkouski <Jalapa10@yandex.ru>
2023-03-13 23:03:05 +0300

    Create struct Post and List in ContentView

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -9,18 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("H4X0R NEWS")
         }
-        .padding()
     }
 }
+
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola"),
+]
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+
